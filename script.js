4bbf7b5ef7fd50c1354191998fca0771a8660357
jaluiovilash <ovilashjalui@gmail.com>
2024-05-21 22:49:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.buttonss')
+console.log(buttons);
+
+const body = document.querySelector('body')
+
+buttons.forEach(function (button) {
+    console.log(button)
+    button.addEventListener("click", (e) => {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        } if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
